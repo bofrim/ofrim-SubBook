79ca8e17aaff8899d04a9b22913623e7f12886a2
Brad Ofrim <bofrim@gmail.com>
2018-02-03 12:49:32 -0700

    Adds addapter to the list activity

--- app/src/main/java/com/ofrim/ofrim_subbook/SubscriptionListActivity.java
+++ app/src/main/java/com/ofrim/ofrim_subbook/SubscriptionListActivity.java
@@ -3,12 +3,23 @@ package com.ofrim.ofrim_subbook;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+
+import java.util.List;
 
 public class SubscriptionListActivity extends ListActivity {
+
+    private Subscription[] subscriptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subscription_list);
+
+        // Associate the subscription data with the interface elements
+        SubscriptionRowAdapter subscriptionArrayAdapter = new SubscriptionRowAdapter(this, subscriptions);
+        setListAdapter(subscriptionArrayAdapter);
     }
 }
