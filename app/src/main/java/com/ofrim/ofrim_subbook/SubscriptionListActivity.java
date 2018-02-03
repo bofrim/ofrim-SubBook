0831af1b2147d05543c5a4fe631085e506befef3
Brad Ofrim <bofrim@gmail.com>
2018-02-02 21:04:25 -0700

    Starting Point.

---
+++ app/src/main/java/com/ofrim/ofrim_subbook/SubscriptionListActivity.java
@@ -0,0 +1,14 @@
+package com.ofrim.ofrim_subbook;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubscriptionListActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscription_list);
+    }
+}
